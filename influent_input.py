8e4f6e1b086ea7fab1e7492bcc5ad3100320d21a
luisacarbo <luisacarbo13@gmail.com>
2022-09-09 13:02:51 +0200

    update

--- influent_input.py
+++ influent_input.py
@@ -1,15 +1,18 @@
 import numpy as np
 import pandas as pd
 
-dg = pd.read_excel("input\input_data.xlsx", sheet_name='influent input', header=None)
+dg = pd.read_excel("input\input_data_NugrohoSantoso.xlsx", sheet_name='influent input', header=None)
 
-So = dg[5][2]
-Xo = dg[5][3]
+Sinitial = dg[5][2]
+Xinitial = dg[5][3]
 Eo = 0.01
 Mo = 1e-6
 Ro = dg[5][4]
 Tw = dg[5][5]  + 273.15
 To = dg[5][6] + 273.15
+
+So = Sinitial/Xinitial
+Xo = Xinitial/Xinitial
 
 print('\nInfluent Data of reactor:')
 print(f'Substrate: {So}')
